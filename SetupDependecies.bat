d1f325c50bd75c19bbdae86d1bd0ca84b9899841
André Viana <andreanaiv@gmail.com>
2024-01-04 17:07:01 -0300

    Dependecies check

---
+++ SetupDependecies.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set gitUrl=https://github.com/4ndsv/
+set branchName=main
+set repoList=Calculator Viewer
+
+echo Checking dependecies...
+
+(for %%r in (%repoList%) do ( 
+   start cmd /k SetupGit %gitUrl% %%r %branchName% %%r
+   echo %%r Done!
+))
+
+pause >nul
---
+++ SetupGit.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set gitUrl=%1
+set repoName=%2
+set branchName=%3
+
+echo URL:%gitUrl%,Repo:%repoName%,Branch:%branchName%
+
+cd..
+
+if exist %repoName%\ (
+  echo Updating Repo %repoName%
+  cd %repoName%
+  git checkout %branchName%
+  git fetch
+  git pull
+) else (
+  echo Repo %repoName% does not exist in parent folder.
+  git clone %gitUrl%%repoName%.git
+  cd %repoName%
+  git checkout %branchName%
+)
